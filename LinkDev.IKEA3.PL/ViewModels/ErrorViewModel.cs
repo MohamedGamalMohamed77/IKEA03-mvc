1924ca387c00b01738eabab53e9a78b72a22c429
mohamedgamalmohamedabdelnaby <mohamedgamalabdelnaby599@gmail.com>
2024-09-19 22:20:12 +0300

    Part 04 MVC Project Structure

---
+++ LinkDev.IKEA3.PL/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LinkDev.IKEA3.PL.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
