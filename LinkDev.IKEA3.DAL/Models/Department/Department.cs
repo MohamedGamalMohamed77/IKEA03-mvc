c6cf18f06b47b5db29f88290e48f1a711825bfed
mohamedgamalmohamedabdelnaby <mohamedgamalabdelnaby599@gmail.com>
2024-09-20 23:07:02 +0300

    Part 05 Department Controller - Create

--- LinkDev.IKEA3.DAL/Models/Department/Department.cs
+++ LinkDev.IKEA3.DAL/Models/Department/Department.cs
@@ -10,7 +10,7 @@ namespace LinkDev.IKEA3.DAL.Models.Department
     {
         public string Name { get; set; } = null!;
         public string Code { get; set; } = null!;
-        public string Description { get; set; } = null!;
+        public string? Description { get; set; } = null!;
         public DateOnly CreationDate { get; set; }
 
 
--- LinkDev.IKEA3.PL/Controllers/DepartmentController.cs
+++ LinkDev.IKEA3.PL/Controllers/DepartmentController.cs
@@ -7,10 +7,15 @@ namespace LinkDev.IKEA3.PL.Controllers
     public class DepartmentController : Controller
     {
         private readonly IDepartmentService _departmentService;
-       
+        private readonly ILogger<DepartmentController> _logger;
+        private readonly IWebHostEnvironment _environment;
 
-        public DepartmentController(IDepartmentService departmentService)
+        public DepartmentController(IDepartmentService departmentService,
+            ILogger<DepartmentController> logger,
+            IWebHostEnvironment environment)
         {
+            _logger = logger;
+            _environment = environment;
             _departmentService = departmentService;
 
         }
@@ -20,7 +25,61 @@ namespace LinkDev.IKEA3.PL.Controllers
             var departments = _departmentService.GetAllDepartments();
             return View(departments);
         }
-       
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreatedDepartmentDto department)
+        {
+            if (!ModelState.IsValid)
+                return View(department);
+
+            var message = string.Empty;
+
+            try
+            {
+                var result = _departmentService.CreatedDepartment(department);
+                if (result > 0)
+                    return RedirectToAction(nameof(Index));
+                else
+                {
+                    message = "Department isn't Created";
+                    ModelState.AddModelError(string.Empty, message);
+                    return View(department);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                if (_environment.IsDevelopment())
+                {
+                    message = ex.Message;
+                    return View(department);
+                }
+                else
+                {
+                    message = "Department isn't Created";
+                    return View("Error", message);
+                }
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (id is null)
+                return BadRequest();
+
+            var department = _departmentService.GetDepartmentById(id.Value);
+
+            if (department is null)
+                return NotFound();
+
+            return View(department);
+        }
 
     }
 }
