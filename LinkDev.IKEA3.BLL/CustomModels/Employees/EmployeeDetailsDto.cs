58efdd4e38aabbe6d3ea0ed51ba25fd987ac98a1
mohamedgamalmohamedabdelnaby <mohamedgamalabdelnaby599@gmail.com>
2024-09-29 18:09:42 +0300

    Part 06 Refactor DetailsEmployee Action - Show Image

--- LinkDev.IKEA3.BLL/CustomModels/Employees/EmployeeDetailsDto.cs
+++ LinkDev.IKEA3.BLL/CustomModels/Employees/EmployeeDetailsDto.cs
@@ -8,23 +8,23 @@ using System.Threading.Tasks;
 
 namespace LinkDev.IKEA3.BLL.CustomModels.Employees
 {
-	public class EmployeeDetailsDto
-	{
-		public int Id { get; set; }
-		public string Name { get; set; } = null!;
-		public int? Age { get; set; }
-		public string? Address { get; set; }
-		[DataType(DataType.Currency)]
-		public decimal Salary { get; set; }
-		[DataType(DataType.EmailAddress)]
-		public string? Email { get; set; }
-		public DateOnly HiringDate { get; set; }
-		public string? PhoneNumber { get; set; }
+    public class EmployeeDetailsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int? Age { get; set; }
+        public string? Address { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Salary { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        public DateOnly HiringDate { get; set; }
+        public string? PhoneNumber { get; set; }
 
-		[Display(Name = "Is Active")]
-		public bool IsActive { get; set; }
-		public EmpType EmployeeType { get; set; } 
-		public Gender Gender { get; set; }
+        [Display(Name = "Is Active")]
+        public bool IsActive { get; set; }
+        public EmpType EmployeeType { get; set; }
+        public Gender Gender { get; set; }
 
         #region Adminstration
         public int CreatedBy { get; set; }
@@ -33,7 +33,7 @@ namespace LinkDev.IKEA3.BLL.CustomModels.Employees
         public DateTime LastModifiedOn { get; set; }
 
         public string? Department { get; set; }
-
+        public string? Image { get; set; }
 
 
         #endregion
