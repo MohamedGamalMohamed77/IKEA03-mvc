216a36bca2319ecc7fa36229f637ca3712925415
mohamedgamalmohamedabdelnaby <mohamedgamalabdelnaby599@gmail.com>
2024-09-30 22:55:37 +0300

    Part 02 Microsoft Identity Package

--- LinkDev.IKEA3.DAL/Presistance/Data/ApplicationDbContext.cs
+++ LinkDev.IKEA3.DAL/Presistance/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using LinkDev.IKEA3.DAL.Models.Departments;
 using LinkDev.IKEA3.DAL.Models.Employees;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,16 +11,18 @@ using System.Threading.Tasks;
 
 namespace LinkDev.IKEA3.DAL.Presistance.Data
 {
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //=> optionsBuilder.UseSqlServer("Server= . ; Database = MVCApplication3G01; Trusted_Connection= True; TrustServerCertificate=True ; MultipleActiveResultSets = True ; Encrypt= False ; ");
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+		//protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		//=> optionsBuilder.UseSqlServer("Server= . ; Database = MVCApplication3G01; Trusted_Connection= True; TrustServerCertificate=True ; MultipleActiveResultSets = True ; Encrypt= False ; ");
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-
-        public DbSet<Department> Departments { get; set; }
-        public DbSet<Employee> Employees { get; set; }
-    }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{ 
+			base.OnModelCreating(modelBuilder); 
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+		public DbSet<Department> Departments { get; set; }
+		public DbSet<Employee> Employees { get; set; }
+	}
 }
